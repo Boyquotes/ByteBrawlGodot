2a39bf2970250ae0b3985f47ebd0de4c91917b88
Conti Aymeric <aymeric.conti@student-cs.fr>
2023-03-28 22:50:46 +0200

    update dash

--- Gameplay/Actions/Dash.gd
+++ Gameplay/Actions/Dash.gd
@@ -1,8 +1,22 @@
 class_name ActionDash
 extends Action
 
-func _init():
+var distance: float
+
+func _init(distance: float):
 	super._init()
 	
 	type = EActionType.cast
+	self.distance = distance
 
+func activate(player: Node):
+	if not canBeCast(player):
+		return
+	
+	player.move_and_slide(PlayerInput.get_direction() * distance)
+
+func cancel(player: Node):
+	pass
+
+func get_variables_to_set() -> Array[String]:
+	return ["distance"]
