619a448bd7125c127cb6669f65d41a80248dd170
Conti Aymeric <aymeric.conti@student-cs.fr>
2023-04-11 14:11:19 +0200

    can move actions in UI

--- UI/Input/FieldEnum.gd
+++ UI/Input/FieldEnum.gd
@@ -2,25 +2,28 @@ class_name FieldEnum
 extends Field
 
 var popup: PopupMenu
-var items_string: Array
+
+var button: MenuButton
 
 func init(field_name: String, pretty_name: String, getter: Callable, setter: Callable, items_string: Array = []):
-	self.items_string = items_string
+	button = get_node("Button")
 	
-	popup = (self as Control as MenuButton).get_popup()
+	popup = button.get_popup()
 	
 	popup.connect("id_pressed", on_item_pressed)
 	for item_string in items_string:
 		popup.add_item(item_string)
 	
+	(get_node("Panel/Label") as Label).text = pretty_name
+	
 	super.init(field_name, pretty_name, getter, setter)
 
 func _ready():
-	self.text = getter.call()
+	button.text = getter.call()
 
 func on_item_pressed(item_id: int):
 	update_value(popup.get_item_text(item_id))
 
 func update_value(field_value):
 	super.update_value(field_value)
-	self.text = getter.call()
+	button.text = getter.call()
