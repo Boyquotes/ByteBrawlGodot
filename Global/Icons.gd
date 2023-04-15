a363774824fa555e9aa9cf7b3fcc9411a808b22d
mconti <mc.heux@gmail.com>
2023-04-15 23:15:01 +0200

    rename fix

--- Global/Icons.gd
+++ Global/Icons.gd
@@ -5,7 +5,7 @@ var Icons = load("res://Assets/Icons/icons.tres") as SpriteFrames
 func get_texture(name: String, index: int):
 	return Icons.get_frame_texture(name, index)
 
-func get_all_texture(name: String) -> Array[Texture2D]:
+func get_all_textures(name: String) -> Array[Texture2D]:
 	var ret = []
 	for i in Icons.get_frame_count(name):
 		ret.append(get_texture(name, i))
