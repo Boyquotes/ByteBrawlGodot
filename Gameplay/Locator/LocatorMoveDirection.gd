d88f4edc81cb8fb51f6fdcd65aaeb732e32830f9
mconti <mc.heux@gmail.com>
2023-04-16 11:15:00 +0200

    locator visibile field

--- Gameplay/Locator/LocatorMoveDirection.gd
+++ Gameplay/Locator/LocatorMoveDirection.gd
@@ -4,10 +4,11 @@ extends BaseNode2D
 
 var TEXTURE = load("res://Assets/NinjaAdventure/HUD/Kunai.png")
 @export var distance: float = 64.0
+var sprite: Sprite2D
 
 func _ready():
 	name = "target_locator"
-	var sprite = Sprite2D.new()
+	sprite = Sprite2D.new()
 	sprite.texture = TEXTURE
 	sprite.rotation_degrees = -130
 	add_child(sprite)
@@ -16,7 +17,7 @@ func _physics_process(delta_time):
 	if not get_parent():
 		return
 	position = get_target_normalized_from_parent() * distance
-	hide() if position.is_zero_approx() else show()
+	sprite.hide() if position.is_zero_approx() else sprite.show()
 	look_at(get_parent().position)
 
 func get_target_normalized_from_parent() -> Vector2:
