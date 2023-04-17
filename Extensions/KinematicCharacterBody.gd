dece5ab4f288e29bd3398e774525e721f9348640
Conti Aymeric <aymeric.conti@student-cs.fr>
2023-04-17 22:32:32 +0200

    add frottement

--- Extensions/KinematicCharacterBody.gd
+++ Extensions/KinematicCharacterBody.gd
@@ -1,6 +1,10 @@
 extends CharacterBody2D
 class_name KinematicCharacterBody
 
+const g: float = 9.81
+const f_d: float = .8
+const f_s: float = 1.
+
 @export var mass: float = 1.
 
 var acceleration: Vector2 = Vector2.ZERO
@@ -12,5 +16,11 @@ func apply_impulse(impulse: Vector2):
 	velocity += mass * impulse
 
 func _physics_process(delta_time):
-	velocity += acceleration * delta_time
+	var f: float = f_s if velocity.is_zero_approx() else f_d
+	
+	if acceleration.length_squared() >= (f * g)**2:
+		velocity += (acceleration - f*g*velocity.normalized()) * delta_time
+	else:
+		velocity = Vector2.ZERO
+	
 	move_and_slide()
