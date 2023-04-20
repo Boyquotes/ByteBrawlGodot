7ca914eb50cee1f15156dfce81264d9364c58bd3
mconti <mc.heux@gmail.com>
2023-04-20 20:16:34 +0200

    Spells import + EploseFireBall

--- Global/Spells.gd
+++ Global/Spells.gd
@@ -1,6 +1,21 @@
-class_name Spells
+extends Node
+var scene_path: Dictionary
 
-const scene_path = {
-	"FireBall": "res://Entities/Spells/SpellFireBall.tscn",
-	"Pull": "res://Entities/Spells/SpellPull.tscn"
-}
+func dir_contents(path: String):
+	var dir = DirAccess.open(path)
+	if not dir: return
+	dir.list_dir_begin()
+	while add_spell_to_path(dir.get_next(), path): pass
+
+func add_spell_to_path(name: String, path: String) -> bool:
+	if name == "": return false
+	var key = name.replace(".tscn", "").replace("Spell", "")
+	scene_path[key] = "%s/%s" % [ path, name ]
+	return true
+
+func init_spells_path():
+	dir_contents("res://Entities/Spells/")
+
+func _init():
+	init_spells_path()
+
